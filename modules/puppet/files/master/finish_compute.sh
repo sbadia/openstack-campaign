cdd16eb0b5e823c24504414a5ed4c532c3fa6fcf
Sebastien Badia <seb@sebian.fr>
2012-07-12 18:51:44 +0200

    update maste/compute finish

---
+++ modules/puppet/files/master/finish_compute.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo -en '--flat_interface=eth0\n--flat_injected=True\n' >> /etc/nova/nova.conf
+sed -e 's/FlatManager/FlatDHCPManager/' -i /etc/nova/nova.conf
+/etc/init.d/nova-compute restart
