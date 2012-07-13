e85d7d166518b1d2e8c413314693344b2979fb28
Sebastien Badia <seb@sebian.fr>
2012-07-13 18:06:08 +0200

    update display, restart volumes

--- modules/puppet/files/master/finish_compute.sh
+++ modules/puppet/files/master/finish_compute.sh
@@ -2,3 +2,4 @@
 echo -en '--flat_interface=eth0\n--flat_injected=True\n' >> /etc/nova/nova.conf
 sed -e 's/FlatManager/FlatDHCPManager/' -i /etc/nova/nova.conf
 /etc/init.d/nova-compute restart
+/etc/init.d/nova-volume restart
