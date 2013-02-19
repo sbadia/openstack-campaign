9b50f093094b2147bc67754e08ea568567cb080b
Sebastien Badia <seb@sebian.fr>
2013-02-19 19:14:58 +0100

    [modules] Lint puppet (add master+storeconfigs) 3/3

---
+++ modules/puppet/manifests/master.pp
@@ -0,0 +1,70 @@
+# Module:: puppet
+# Manifest:: master.pp
+#
+# Author:: Sebastien Badia (<seb@sebian.fr>)
+# Date:: Thu May 31 18:36:30 +0200 2012
+# Maintainer:: Sebastien Badia (<seb@sebian.fr>)
+#
+
+# Class:: puppet::master
+#
+#
+class puppet::master {
+  require 'puppet'
+
+  package {
+    ['puppetmaster','libmysql-ruby','build-essential','libmysqlclient-dev','libactiverecord-ruby']:
+      ensure    => installed;
+    'mysql':
+      ensure    => installed,
+      provider  => gem,
+      require   => [Package['libmysqlclient-dev'],File['/etc/gemrc']];
+    'activerecord':
+      ensure    => '3.0.11',
+      provider  => gem,
+      require   => File['/etc/gemrc'];
+  }
+
+  File['/etc/puppet/puppet.conf'] { source  => 'puppet:///modules/puppet/master/puppet.conf' }
+
+  include 'mysql'
+
+  class { 'mysql::server': }
+
+  mysql::db {
+    'puppet':
+      user     => 'puppet',
+      password => 'puppet',
+      host     => 'localhost',
+      grant    => ['all'],
+      require  => Class['mysql::config'],
+   }
+
+  file {
+    '/etc/puppet/manifests/install.pp':
+      ensure  => file,
+      source  => 'puppet:///modules/puppet/master/install.pp',
+      owner   => root,
+      group   => root,
+      mode    => '0644',
+      require => Package['puppetmaster'];
+    '/etc/puppet/manifests/openstack.pp':
+      ensure  => file,
+      source  => 'puppet:///modules/puppet/master/openstack.pp',
+      owner   => root,
+      group   => root,
+      mode    => '0644',
+      require => Package['puppetmaster'];
+    '/etc/puppet/manifests/site.pp':
+      ensure  => link,
+      target  => '/etc/puppet/manifests/openstack.pp',
+      require => File['/etc/puppet/manifests/openstack.pp'];
+    '/etc/puppet/autosign.conf':
+      ensure  => file,
+      source  => 'puppet:///modules/puppet/master/autosign.conf',
+      owner   => root,
+      group   => root,
+      mode    => '0644',
+      require => Package['puppetmaster'];
+  }
+} # Class:: puppet::master
